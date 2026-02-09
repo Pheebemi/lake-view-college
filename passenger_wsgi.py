30e7e39aa6516c7eb73081b1ac16edbad8d5b044
pheebemi <lemuelemmanuel29@gmail.com>
2026-02-09 23:12:30 +0100

    sa.sa

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,17 @@
 """
 cPanel / Passenger WSGI entry point for LakeView College.
 Place this file in the same directory as manage.py (application root).
-Adjust INTERP if your cPanel virtualenv path is different.
 """
 import sys
 import os
 
-# Path to cPanel-created virtualenv (common: app_root/virtualenv)
-# If your app root is e.g. public_html/lakeview, use: os.path.join(os.environ['HOME'], 'public_html', 'lakeview', 'virtualenv')
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
-VENV_PYTHON = os.path.join(APP_DIR, 'virtualenv', 'bin', 'python3')
+
+# cPanel often creates venv at ~/virtualenv/<app_name>/<version>/bin/python3 (change 3.11 if you use another version)
+HOME = os.environ.get('HOME', '')
+VENV_PYTHON = os.path.join(HOME, 'virtualenv', 'lakeview', '3.11', 'bin', 'python3')
+if not os.path.exists(VENV_PYTHON):
+    VENV_PYTHON = os.path.join(APP_DIR, 'virtualenv', 'bin', 'python3')
 if os.path.exists(VENV_PYTHON) and sys.executable != VENV_PYTHON:
     os.execl(VENV_PYTHON, VENV_PYTHON, *sys.argv)
 
