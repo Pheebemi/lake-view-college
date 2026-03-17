011c0c812863e9bc04f819174274505c117afb34
pheebemi <lemuelemmanuel29@gmail.com>
2026-03-17 15:07:36 +0100

    fix

--- accounts/models.py
+++ accounts/models.py
@@ -285,7 +285,7 @@ class Course(models.Model):
         ('second', 'Second Semester'),
     )
 
-    code = models.CharField(max_length=10, unique=True)
+    code = models.CharField(max_length=20, unique=True)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     credits = models.IntegerField()
